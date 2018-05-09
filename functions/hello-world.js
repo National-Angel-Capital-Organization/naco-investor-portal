1ca8082b4f4a817d2f6c97cfbc65c49f149a0b08
Bryan Hunter <bdhunter3141@gmail.com>
2018-05-09 12:02:39 -0400

    More troubleshooting

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -75,18 +75,19 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.handler = handler;
 function handler(event, context, callback) {
-  console.log(context);
-  console.log(event.headers.host);
-  let userEmail = '';
-  if (event.headers.host === 'localhost:8000') {
-    userEmail = 'bhunter@nacocanada.com';
-  } else {
-    userEmail = context.clientContext.identity.user.email;
-  }
+  console.log("this is the context: ", context);
+  console.log("this is the event: ", event);
+  // let userEmail = ''
+  // if (event.headers.host === 'localhost:8000') {
+  //   userEmail = 'bhunter@nacocanada.com'
+  // } else {
+  //   userEmail = 'troubleshooting'
+  // }
+  // console.log(userEmail)
 
   callback(null, {
     statusCode: 200,
-    body: `Hello, ${userEmail}`
+    body: `Hello, world`
   });
 }
 
--- src/functions/hello-world.js
+++ src/functions/hello-world.js
@@ -1,13 +1,13 @@
 export function handler(event, context, callback) {
-  // console.log("this is the context: ", context)
-  // console.log("this is the event: ", event)
-  // // let userEmail = ''
-  // // if (event.headers.host === 'localhost:8000') {
-  // //   userEmail = 'bhunter@nacocanada.com'
-  // // } else {
-  // //   userEmail = 'troubleshooting'
-  // // }
-  // // console.log(userEmail)
+  console.log("this is the context: ", context)
+  console.log("this is the event: ", event)
+  // let userEmail = ''
+  // if (event.headers.host === 'localhost:8000') {
+  //   userEmail = 'bhunter@nacocanada.com'
+  // } else {
+  //   userEmail = 'troubleshooting'
+  // }
+  // console.log(userEmail)
 
   callback(null, {
     statusCode: 200,
