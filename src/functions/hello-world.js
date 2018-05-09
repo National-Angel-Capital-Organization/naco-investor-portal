35537e88e3db1cc44cabb79341879e9c0ea72f1a
Bryan Hunter <bdhunter3141@gmail.com>
2018-05-09 11:23:44 -0400

    More troubleshooting

--- src/functions/hello-world.js
+++ src/functions/hello-world.js
@@ -1,14 +1,16 @@
 export function handler(event, context, callback) {
   console.log("this is the context: ", context)
+  console.log("this is the event: ", event)
   let userEmail = ''
   if (event.headers.host === 'localhost:8000') {
     userEmail = 'bhunter@nacocanada.com'
   } else {
     userEmail = 'troubleshooting'
   }
+  console.log(userEmail)
 
   callback(null, {
     statusCode: 200,
-    body: `Hello, ${userEmail}`
+    body: `Hello, world`
   });
 }
