620a051aa1486012737df58fa2fc077bb3165f2f
Bryan Hunter <bdhunter3141@gmail.com>
2018-06-27 13:02:40 -0400

    set up and styled dropdowns for years for both portals.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { navigateTo } from "gatsby-link"
+import SelectField from 'material-ui/SelectField'
+import MenuItem from 'material-ui/MenuItem'
 import PremoneyValueChart from '../components/general-charts/premoney-value-chart'
 import TotalInvestmentNumberChart from '../components/general-charts/total-investment-number-chart'
 import TotalInvestmentDollarChart from '../components/general-charts/total-investment-dollar-chart'
@@ -11,13 +14,44 @@ defaults.scale.ticks.beginAtZero = true;
 
 export default class IndexPage extends Component {
 
+  state = {
+    currentYear: 'all years.',
+    error: '',
+    yearList: ['all years.', '2018.', '2017.', '2016.']
+  }
+
+  handleDropdownChange = (event, index, value) => {
+    this.setState({ currentYear: value })
+  }
 
 
   render() {
     return (
       <div>
         <h1>General Dashboard</h1>
-        <p>Here you can find general investment information.</p>
+        <p>Here you can find general investment information from
+          <SelectField
+            value={this.state.currentYear}
+            style={{ bottom: -26, width: '140px', paddingLeft: '5px', font: '18px "Roboto", sans-serif', fontWeight: '500' }}
+            underlineStyle={{ borderColor: '#0079c1' }}
+            iconStyle={{ fill: '#0079c1' }}
+            selectedMenuItemStyle={{ color: 'rgba(0, 0, 0, 0.8)' }}
+            onChange={this.handleDropdownChange}
+            labelStyle={this.styles}
+            errorText={this.state.error}
+          >
+            {this.state.yearList.map(i => (
+              <MenuItem
+                style={{ paddingLeft: '5px', font: '18px "Roboto", sans-serif', color: 'rgba(0, 0, 0, 0.5)' }}
+                width={'140px'}
+                key={i}
+                value={i}
+                primaryText={i}
+                name='Years'
+              />
+            ))}
+          </SelectField>
+        </p>
         <div className='chart-wrapper'>
           <div className='chart-container doughnut'>
             <TotalInvestmentNumberChart />
--- src/pages/personal-dashboard.js
+++ src/pages/personal-dashboard.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import { defaults } from 'react-chartjs-2';
+import { navigateTo } from "gatsby-link"
+import SelectField from 'material-ui/SelectField'
+import MenuItem from 'material-ui/MenuItem'
 import AverageInvestmentDollarChart from '../components/personal-charts/average-investment-dollar-chart'
 import AverageInvestmentNumberChart from '../components/personal-charts/average-investment-number-chart'
 import InvestmentDollarChart from '../components/personal-charts/investment-dollar-chart'
@@ -10,14 +13,45 @@ import SectorNumberChart from '../components/personal-charts/sector-number-chart
 //Set all charts to begin at Zero
 defaults.scale.ticks.beginAtZero = true;
 
-
 export default class PersonalDashboard extends Component {
-  state = {}
+
+  state = {
+    currentYear: 'all years.',
+    error: '',
+    yearList: ['all years.', '2018.', '2017.', '2016.']
+  }
+
+  handleDropdownChange = (event, index, value) => {
+    this.setState({ currentYear: value })
+  }
+
   render() {
     return (
       <div>
         <h1>Personal Dashboard</h1>
-        <p>Here you can find personal investment information.</p>
+        <p>Here you can find personal investment information from
+          <SelectField
+            value={this.state.currentYear}
+            style={{ bottom: -26, width: '140px', paddingLeft: '5px', font: '18px "Roboto", sans-serif', fontWeight: '500' } }
+            underlineStyle={{ borderColor: '#0079c1' }}
+            iconStyle={{ fill: '#0079c1' }}
+            selectedMenuItemStyle={{ color: 'rgba(0, 0, 0, 0.8)'}}
+            onChange={this.handleDropdownChange}
+            labelStyle={this.styles}
+            errorText={this.state.error}
+          >
+            {this.state.yearList.map(i => (
+              <MenuItem
+                style={{ paddingLeft: '5px', font: '18px "Roboto", sans-serif', color: 'rgba(0, 0, 0, 0.5)' } }
+                width={'140px'}
+                key={i}
+                value={i}
+                primaryText={i}
+                name='Years'
+              />
+            ))}
+          </SelectField>
+          </p>
         <div className='chart-wrapper'>
           <div className='chart-container doughnut'>
             <InvestmentDollarChart />
