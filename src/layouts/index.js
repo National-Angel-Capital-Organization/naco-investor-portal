af6cc2cb8082e2dd3374426395af4471545e0395
Bryan Hunter <bdhunter3141@gmail.com>
2018-09-17 13:11:21 -0400

    Testing new registration system.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -131,7 +131,7 @@ export default class Layout extends Component {
       }
       )
         .then((res) => {
-          console.log(res)
+          return res.data
         })
 
         .catch(error => {
